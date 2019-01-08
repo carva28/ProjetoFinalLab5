5a63aaa0241f5147d0d270cf418eda262a145757
Sofia Ribeiro <sribeiro@ubiwhere.pt>
2019-01-08 20:34:52 +0000

    commit final

--- src/index.js
+++ src/index.js
@@ -6,22 +6,21 @@ import firebase from 'firebase';
 import * as serviceWorker from './serviceWorker';
 
 export const initializeFirebase = () => {
-    firebase.initializeApp({
-      apiKey: "AIzaSyAtYRTVptt8VHYLxUOe8wrztdw3Pud9Ruw",
-      authDomain: "projeto-4a0db.firebaseapp.com",
-      databaseURL: "https://projeto-4a0db.firebaseio.com",
-      projectId: "projeto-4a0db",
-      messagingSenderId: "1025840405057"
-    })
-  }
+  firebase.initializeApp({
+    apiKey: "AIzaSyAtYRTVptt8VHYLxUOe8wrztdw3Pud9Ruw",
+    authDomain: "projeto-4a0db.firebaseapp.com",
+    databaseURL: "https://projeto-4a0db.firebaseio.com",
+    projectId: "projeto-4a0db",
+    messagingSenderId: "1025840405057"
+  })
+}
 
 initializeFirebase();
-  
-/* if (window.innerWidth <= 500) { */
+
+if (window.innerWidth <= 500) {
   ReactDOM.render(<App />, document.getElementById('root'));
-  /* } else if (window.innerWidth > 500) {
-      ReactDOM.render(<Desktop />, document.getElementById('root'));
-  } */
+} else if (window.innerWidth > 500) {
+  ReactDOM.render(<Desktop />, document.getElementById('root'));
+}
 
 serviceWorker.unregister();
-
